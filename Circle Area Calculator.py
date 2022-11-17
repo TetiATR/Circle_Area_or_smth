6dbe6c939a04b4b4b837551096ab0a9b3ce5a928
ratiubogdan <ratiubogdan@yahoo.com>
2022-11-17 18:37:44 +0100

    nvm, messed up first commit

--- Circle Area Calculator.py
+++ Circle Area Calculator.py
@@ -1,3 +1,8 @@
-print("Hello... How big is the radius of the circle you want to measure????")
-Ca = input()
-print("So the area of the circle is ", int(Ca) * int(Ca) * 3.14, "cm²", sep="")
+try:
+    print("Hello... How big is the radius of the circle you want to measure????")
+    Ca = input()
+    print("So the area of the circle is ", int(Ca) * int(Ca) * 3.14, "cm²", sep="")
+except ValueError:
+    print("You Can't Multiply Letters, or Decimal Numbers (Im Sure You Could But Im Too Lazy To Do That) ·_·")
+except :
+    print("N O")
