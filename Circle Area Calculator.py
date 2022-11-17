d315ab93ccbc111080859a60f6a8f9db940c685c
ratiubogdan <ratiubogdan@yahoo.com>
2022-11-17 18:39:31 +0100

    I Got Homework Soooooooooooooooooooooooo...

--- Circle Area Calculator.py
+++ Circle Area Calculator.py
@@ -1,7 +1,7 @@
 try:
     print("Hello... How big is the radius of the circle you want to measure????")
     Ca = input()
-    print("So the area of the circle is ", int(Ca) * int(Ca) * 3.14, "cm²", sep="")
+    print("So the area of the circle is ", int(Ca) * int(Ca) * 3.14, "cm².", sep="")
 except ValueError:
     print("You Can't Multiply Letters, or Decimal Numbers (Im Sure You Could But Im Too Lazy To Do That) ·_·")
 except :
